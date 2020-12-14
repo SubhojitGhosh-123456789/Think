075dbcea3bc0cd5f14dbb90e935bbf02d98527f3
SubhojitGhosh-123456789 <69459860+SubhojitGhosh-123456789@users.noreply.github.com>
2020-12-14 17:25:03 +0530

    Update WriteStoryScreen.js

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -8,6 +8,8 @@ import {
   StyleSheet,
   Button,
   ScrollView,
+  ToastAndroid,
+  KeyboardAvoidingView,
 } from "react-native";
 
 import db from "../config";
@@ -29,61 +31,71 @@ export default class WriteStoryScreen extends React.Component {
       Author: this.state.author,
       Story: this.state.story,
     });
-    alert("Thank You For Submitting Your Story.");
+    ToastAndroid.show(
+      "Thank You For Submitting Your Story.",
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM
+    );
     this.setState({ title: "", author: "", story: "" });
   };
   render() {
     return (
-      <ScrollView>
-        <View style={{ marginTop: 20 }}>
-          <Text style={styles.text}>Think 🤔</Text>
-        </View>
-        <Text
-          style={{
-            textAlign: "center",
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "rgb(255,51,51)",
-          }}
+      <ScrollView style={styles.container}>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={"padding"}
+          enabled
         >
-          Write A Story
-        </Text>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.text}>Think 🤔</Text>
+          </View>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 30,
+              fontWeight: "bold",
+              color: "rgb(255,51,51)",
+            }}
+          >
+            Write A Story
+          </Text>
 
-        <TextInput
-          style={styles.inputBox}
-          placeholder="Story Title"
-          onChangeText={(text) => {
-            this.setState({ title: text });
-          }}
-          value={this.state.title}
-        />
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Story Title"
+            onChangeText={(text) => {
+              this.setState({ title: text });
+            }}
+            value={this.state.title}
+          />
 
-        <TextInput
-          style={styles.inputBox}
-          placeholder="Author"
-          onChangeText={(text) => {
-            this.setState({ author: text });
-          }}
-          value={this.state.author}
-        />
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Author"
+            onChangeText={(text) => {
+              this.setState({ author: text });
+            }}
+            value={this.state.author}
+          />
 
-        <TextInput
-          style={styles.textArea}
-          underlineColorAndroid="transparent"
-          placeholder="Write Your Story"
-          placeholderTextColor="grey"
-          numberOfLines={10}
-          multiline={true}
-          onChangeText={(text) => {
-            this.setState({ story: text });
-          }}
-          value={this.state.story}
-        />
-        <View style={styles.inputView}>
-          <TouchableOpacity style={styles.submitButton} onPress={this.submit}>
-            <Text style={styles.submitButtonText}>Submit</Text>
-          </TouchableOpacity>
-        </View>
+          <TextInput
+            style={styles.textArea}
+            underlineColorAndroid="transparent"
+            placeholder="Write Your Story"
+            placeholderTextColor="grey"
+            numberOfLines={10}
+            multiline={true}
+            onChangeText={(text) => {
+              this.setState({ story: text });
+            }}
+            value={this.state.story}
+          />
+          <View style={styles.inputView}>
+            <TouchableOpacity style={styles.submitButton} onPress={this.submit}>
+              <Text style={styles.submitButtonText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
       </ScrollView>
     );
   }
@@ -116,7 +128,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     backgroundColor: "white",
-    borderWidth: 3.5,
     borderColor: "magenta",
     marginTop: 20,
     alignSelf: "center",
